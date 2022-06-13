027c60375cbb346880b1591012c62b20477d51e5
Parkeunyong <eunyoung4789@gmail.com>
2022-06-14 01:13:41 +0900

    thx

---
+++ app/src/main/java/com/example/foodrecipe/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.foodrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new CustomAdapter();
+        ListView listView = (ListView) findViewById(R.id.foodrecipe);
+
+        setData();
+
+        listView.setAdapter(adapter);
+    }
+
+    private void setData() {
+        TypedArray arrResId = getResources().obtainTypedArray(R.array.resId);
+        String[] titles = getResources().getStringArray(R.array.title);
+        String[] contents = getResources().getStringArray(R.array.content);
+
+        for (int i = 0; i < arrResId.length(); i++) {
+            DataTransferObject dto = new DataTransferObject();
+            dto.setResId(arrResId.getResourceId(i, 0));
+            dto.setTitle(titles[i]);
+            dto.setContent(contents[i]);
+
+            adapter.addItem(dto);
+        }
+    }
+}
